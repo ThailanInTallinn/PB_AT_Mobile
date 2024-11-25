250d555693196b97534bf8deb477f5603bc646fa
Thailan Gouvea <thailangm@icloud.com>
2024-11-25 16:49:20 +0200

    Adds a header to the project
    It's been also removed a few unnecessary files and
    folders.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Header from "./src/components/header/header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <Header />
     </View>
   );
 }
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#242424",
+    alignItems: "center",
+    //justifyContent: "center",
+    width: "100vw",
+    height: "100vh",
   },
 });
---
+++ src/components/header/header.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.logoPara}>ArleteFlix</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: "black",
+    justifyContent: "center",
+    width: "100%",
+    height: "12%",
+    padding: 10,
+  },
+
+  logoPara: {
+    color: "rgb(172, 19, 19)",
+    fontFamily: " Helvetica, Arial, sans-serif",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+});
